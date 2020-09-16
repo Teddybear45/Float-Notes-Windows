a23f89bdad29383a64dbe735c81e0be85974a221
teddybear45 <tedguan2005@gmail.com>
2020-09-15 20:30:13 -0700

    starting link sqlite

---
+++ FloatLibrary/SqliteDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FloatLibrary
+{
+    public class SqliteDataAccess
+    {
+
+        public static List<SingleNoteStack> LoadNotes()
+        {
+
+        }
+
+        public static void SaveNote(SingleNoteStack note)
+        {
+
+        }
+
+    }
+}
